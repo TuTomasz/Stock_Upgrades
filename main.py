f907b7b6d630dbc21ac40115acda4fd3078dc5dc
Tomasz Turek <ttomaszito@gmail.com>
2021-12-31 11:52:20 -0500

    proxy fetching service

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from src.ratings import Ratings
 from src.other import readQueueFile, appendQueueFile, clearQueueFile, populateQueueFile
-
+from src.proxy_service import ProxyService
 
 dest_dir = os.path.join("Data", "Rating")
 
@@ -12,17 +12,14 @@ AllProxies = []
 if __name__ == "__main__":
 
     # read proxy.txt file
-    with open("Data/proxy/proxy.txt", "r") as infile:
-        proxies = infile.read().splitlines()
-
-    for proxy in proxies:
-        AllProxies.append("http://" + proxy)
+    proxies = ProxyService().fetchProxies()
+    print(proxies)
 
     # clear queue if one is filled
     clearQueueFile()
 
     # obtain new ratings and update
-    Ratings(dest_dir, AllProxies)
+    Ratings(dest_dir, proxies)
 
     # populate queue with new ratings that are of date today
     populateQueueFile("Data/queue/rating_queue.txt")
--- src/proxy_service.py
+++ src/proxy_service.py
@@ -1,17 +1,37 @@
 from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
 
 
 class ProxyService:
     def __init__(self):
         self.proxy_list = []
 
-    def get_proxy_list(self):
+    def getProxyList(self):
         return self.proxy_list
 
-    def fetchProxies():
+    def fetchProxies(self):
 
         url = "https://github.com/TheSpeedX/PROXY-List/blob/master/http.txt"
 
         req = Request(url, headers={"User-Agent": "Mozilla/5.0"})
 
-        webpage = urlopen(req).read().decode("utf-8")
+        response = urlopen(req).read()
+
+        proxy = BeautifulSoup(response, "html.parser")
+
+        for i in proxy.findAll("tr"):
+            self.proxy_list.append("http://" + i.text.replace("\n", ""))
+
+        return self.proxy_list
+
+    def writeProxies(self):
+        with open("data/proxy/proxy.txt", "w") as f:
+            for i in self.proxy_list:
+                f.write(i + "\n")
+
+
+if __name__ == "__main__":
+
+    proxies = ProxyService()
+    proxies.fetchProxies()
+    proxies.writeProxies()
