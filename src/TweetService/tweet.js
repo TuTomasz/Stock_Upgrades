68039cbf8c31c96f62705194b43c12d8448d0094
Tomasz Turek <ttomaszito@gmail.com>
2022-02-04 21:29:04 -0500

    added ability to post messages

--- src/TweetService/tweet.js
+++ src/TweetService/tweet.js
@@ -9,71 +9,75 @@ const AdblockerPlugin = require("puppeteer-extra-plugin-adblocker");
 puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
 
 // import env variables
-require('dotenv').config()
+require("dotenv").config();
 
 //import file system
-const fs = require('fs');
-
-
-
+const fs = require("fs");
 
 puppeteer.launch({ headless: false }).then(async (browser) => {
-    // const page = await browser.newPage();
-    // await page.goto("https://stocktwits.com/");
-  
-    // // Wait for the page to load
-    // await page.waitFor(Math.random() * (3000 - 1000) + 1000);
+  const page = await browser.newPage();
+  await page.goto("https://stocktwits.com/");
 
-    // // Click buttoon with inner text login 
-    
-    // const elements = await page.$x('//*[@id="mainNavigation"]/div[3]/div/div/div[1]/button')
-    // await elements[0].click() 
+  // Wait for the page to load
+  await page.waitFor(Math.random() * (3000 - 1000) + 1000);
 
-    // // Wait for the page to load
-    // await page.waitFor(Math.random() * (3000 - 1000) + 1000);
+  // Click buttoon with inner text login
 
-    // //input username
-    // const username = await page.$x('//*[@id="app"]/div/div/div[4]/div[2]/div/form/div[1]/div[1]/label/input')
-    // username[0].type(process.env.USERNAME)
+  const elements = await page.$x(
+    '//*[@id="mainNavigation"]/div[3]/div/div/div[1]/button'
+  );
+  await elements[0].click();
 
-    // // Wait for the page to load
-    // await page.waitFor(Math.random() * (3000 - 1000) + 1000);
+  // Wait for the page to load
+  await page.waitFor(Math.random() * (3000 - 1000) + 1000);
 
-    // //input password
-    // const password = await page.$x('//*[@id="app"]/div/div/div[4]/div[2]/div/form/div[1]/div[2]/label/input')
-    // password[0].type(process.env.PASSWORD)
+  //input username
+  const username = await page.$x(
+    '//*[@id="app"]/div/div/div[4]/div[2]/div/form/div[1]/div[1]/label/input'
+  );
+  username[0].type(process.env.USERNAME);
 
-    // // CAPCHA problem need manual validation
-    // // const frame = await page.frames().find(f => f.name().startsWith("a-"));
-    // // await frame.waitForSelector('div.recaptcha-checkbox-border').click();
-    // // console.log("Captcha clicked exists!");
+  // Wait for the page to load
+  await page.waitFor(Math.random() * (3000 - 1000) + 1000);
 
-    // await page.waitForNavigation();
+  //input password
+  const password = await page.$x(
+    '//*[@id="app"]/div/div/div[4]/div[2]/div/form/div[1]/div[2]/label/input'
+  );
+  password[0].type(process.env.PASSWORD);
 
+  // CAPCHA problem need manual validation
+  // const frame = await page.frames().find(f => f.name().startsWith("a-"));
+  // await frame.waitForSelector('div.recaptcha-checkbox-border').click();
+  // console.log("Captcha clicked exists!");
 
-    // // Wait for the page to load
-    // await page.waitFor(Math.random() * (3000 - 1000) + 1000);
+  await page.waitForNavigation();
 
-    // Read files from queue file and post them
-    const queue = fs.readFileSync('../../data/Queue/Queue.json', 'utf8').split('\n');
+  // Wait for the page to load
+  await page.waitFor(Math.random() * (3000 - 1000) + 1000);
 
-    try {
-        for (const rating of queue) {
-            let ratingObject = await JSON.parse(rating);
-            console.log(ratingObject);
-        }
-    
-        
-    } catch (error) {
-        console.log(error);
+  //   Read files from queue file and post them
+  const queue = fs
+    .readFileSync("../../data/Queue/Queue.json", "utf8")
+    .split("\n");
+
+  try {
+    for (const rating of queue) {
+      let ratingObject = await JSON.parse(rating);
+      let message = `$${ratingObject.Ticker} ${ratingObject.Rating.Organization} has changed rating to ${ratingObject.Rating.Rating} with a price target change of ${ratingObject.Rating.Target_Change}`;
+
+      console.log(message);
+
+      const post = await page.$x('//*[@id="mainNavigation"]/div[3]/span/button');
+      await post[0].click();
+      const box = await page.$x('//*[@id="app"]/div/div/div[4]/div[2]/div/div[2]/div/div[2]/input')
+      console.log(box);
+      box[0].type(message);
+      break;
     }
+  } catch (error) {
+    console.log(error);
+  }
 
-    console.log("Done");
-   
-    
-
-    
-
-
-  });
-  
+  console.log("Done");
+});
